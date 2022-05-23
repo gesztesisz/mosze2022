6275ead2fca4fd3c56ff2e1d7b7c5286905307ee
Gesztesi Szabolcs <gesztesi11@gmail.com>
2022-05-23 16:13:46 +0200

    fixed close exit

--- main.cpp
+++ main.cpp
@@ -55,6 +55,8 @@ int main(int argc,char* argv[]) {
     else if(command_array[0] == "close"){
       if(tables.size() == 1){
         delete tables[0];
+        tables.erase(tables.begin() + 0);
+        break;
       }
       close_func(tables,command_array,active);
     }
